e6c2a8c5619e67355aaeb094acf63a4c8c35f26b
dalia9999 <daliabedrosyan47@gmail.com>
2025-01-13 17:02:04 +0200

    Added: GameBoard functionality and App.jsx and Player.jsx logical changes

---
+++ src/components/GameBoard/GameBoard.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    const handleSelectSquare = (rowIndex, colIndex) => {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArrays => [...innerArrays])];
+            updatedBoard[rowIndex][colIndex] = {activePlayerSymbol};
+            return updatedBoard;
+        });
+
+        onSelectSquare();
+    }
+
+    return <ol id='game-board'>
+        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            <ol>
+                {row.map((playerSymbol, colIndex) => (
+                    <li key={colIndex}>
+                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol?.activePlayerSymbol}</button>
+                    </li>
+                ))}
+            </ol>
+        </li>)}
+    </ol>
+}
